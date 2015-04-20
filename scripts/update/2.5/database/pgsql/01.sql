9cae4b2c572ba265b4e8f6703c2f1cfcbf2dcb49
Tommy Bolger <tommy@caseyresearch.com>
2015-04-19 20:28:30 -0700

    Added changes not included with the last commit.

--- scripts/update/2.5/database/pgsql/01.sql
+++ scripts/update/2.5/database/pgsql/01.sql
@@ -3,5 +3,5 @@ ALTER TABLE steam_users
 ALTER TABLE steam_users
   ADD CONSTRAINT fk_steam_users_latest_power_ranking_entry_id FOREIGN KEY (latest_power_ranking_entry_id) REFERENCES power_ranking_entries (power_ranking_entry_id) ON UPDATE CASCADE ON DELETE SET NULL;
 
-CREATE UNIQUE INDEX idx_steam_users_latest_power_ranking_entry_id
+CREATE INDEX idx_steam_users_latest_power_ranking_entry_id
    ON steam_users (latest_power_ranking_entry_id ASC NULLS LAST);
