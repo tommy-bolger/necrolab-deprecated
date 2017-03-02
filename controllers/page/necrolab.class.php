1ba1c0ef6d28867ae08226d0a8d953a00da6bf94
Tommy Bolger <tommy.bolger@gmail.com>
2017-03-02 14:34:19 -0800

    Switched page encoding to ISO-8859-1.

--- controllers/page/necrolab.class.php
+++ controllers/page/necrolab.class.php
@@ -97,6 +97,7 @@ extends PageController {
             '/datepicker.css'
         ));
         
+        header('Content-Type: text/html; charset=ISO-8859-1');
         header('Expires: Sat, 26 Jul 1997 05:00:00 GMT');
         header('Last-Modified: ' . gmdate( 'D, d M Y H:i:s') . ' GMT');
         header('Cache-Control: no-store, no-cache, must-revalidate');
