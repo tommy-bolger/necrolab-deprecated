c5d206ff841ada30f965a7f44f28b9c4575570d8
Tommy Bolger <tommy.bolger@gmail.com>
2017-03-03 12:30:22 -0800

    Removed ISO-8859 encoding from pages. Added hard-coded values to dropdown styles.

--- controllers/page/necrolab.class.php
+++ controllers/page/necrolab.class.php
@@ -97,7 +97,6 @@ extends PageController {
             '/datepicker.css'
         ));
         
-        header('Content-Type: text/html; charset=ISO-8859-1');
         header('Expires: Sat, 26 Jul 1997 05:00:00 GMT');
         header('Last-Modified: ' . gmdate( 'D, d M Y H:i:s') . ' GMT');
         header('Cache-Control: no-store, no-cache, must-revalidate');
